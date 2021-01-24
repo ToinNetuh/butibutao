6dc79ce48207d9fcffa53c57229cff4d2ce5fa64
ToinNetuh <gpantonioneto@gmail.com>
2021-01-24 01:14:48 -0300

    Commitur

---
+++ src/help.js
@@ -0,0 +1,153 @@
+const help = (prefix) => {
+	return `
+「 *FI DO TOIN😎🤝* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : Brunin brabo😎🤝
+  ❏ Como instalar o BOT: https://www.youtube.com/watch?v=4cjhlXo9aGo
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}jokerlogo
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}ttp
+  ├─ ❏ ${prefix}herrypotter
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}quotemaker
+◪ *MEDIA*
+  │
+  ├─ ❏ ${prefix}trendtwit
+  ├─ ❏ ${prefix}randomkpop
+  └─ ❏ ${prefix}ytsearch
+◪ *EDUCATION*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  └─ ❏ ${prefix}artinama
+◪ *KERANG AJAIB*
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  ├─ ❏ ${prefix}rate
+  └─ ❏ ${prefix}bisakah
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MÚSICA*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+◪ *DIVERSÃO*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+◪ *INFORMAÇÃO*
+  │
+  ├─ ❏ ${prefix}bahasa
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  ├─ ❏ ${prefix}fakta
+  ├─ ❏ ${prefix}infocuaca
+  ├─ ❏ ${prefix}infogempa
+  ├─ ❏ ${prefix}jadwaltvnow
+  └─ ❏ ${prefix}covid
+◪ *DONO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}exe
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}afk
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2
+`
+}
+
+exports.help = help
